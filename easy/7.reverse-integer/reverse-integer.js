87d7c29b7c9b8ff4f56fcad285f62c589fe80c63
weibin <fatbin37@hotmail.com>
2019-08-05 00:30:55 +0800

    finish no.7

---
+++ easy/7.reverse-integer/reverse-integer.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {
+    const MAX_NUM = Math.pow(2, 31) - 1
+    const MIN_NUM = -Math.pow(2, 31)
+    function isOverflow(num) {
+        if (num > 0) {
+            if (num > ( parseInt(MAX_NUM / 10))) {
+                return true
+            } else {
+                return false
+            }
+        } else {
+            if (num < (parseInt(MIN_NUM / 10))) {
+                return true
+            } else {
+                return false
+            }
+        }
+    }
+    let result = 0
+    while(x) {
+        let pop = x % 10
+        x = parseInt(x / 10)
+        if (isOverflow(result)) {
+            return 0
+        }
+        result =  result * 10 + pop
+    }
+    return result
+};
