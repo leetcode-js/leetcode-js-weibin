219655a64965a063e6bb3d5f351f8771bd528fd4
weibin <fatbin37@hotmail.com>
2019-08-20 22:33:50 +0800

    setup vuepress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,48 @@
+const easy = [
+    '',
+    './1.two-sum/two-sum',
+    './7.reverse-integer/reverse-integer',
+    './9.palindrome-number/palindrome-number'
+]
+const medium = [
+    ''
+]
+const hard = [
+    ''
+]
+
+module.exports = {
+    title: 'Weibin\'s LeetCode',
+    description: 'Weibin 的 LeetCode 题解 -- Javascript 题解',
+    themeConfig: {
+        nav: [
+            { text: '简单', link: '/easy/' },
+            { text: '中等', link: '/medium/' },
+            { text: '困难', link: '/hard/' },
+            { text: 'GitHub', link: 'https://github.com/leetcode-js/leetcode-js-weibin' }
+        ],
+        sidebar: {
+            '/easy/': [
+                {
+                    title: '简单难度',
+                    collapsable: false,
+                    children: easy
+                }
+            ],
+            '/medium/': [
+                {
+                    title: '中等难度',
+                    collapsable: false,
+                    children: medium
+                }
+            ],
+            '/hard/': [
+                {
+                    title: '困难难度',
+                    collapsable: false,
+                    children: hard
+                }
+            ]
+        }
+    }
+}
---
+++ docs/README.md
@@ -0,0 +1,7 @@
+---
+home: true
+heroImage: /leetcode.png
+actionText: 进入题解 →
+actionLink: /easy/
+footer: MIT Licensed | Copyright © 2019-present Weibin
+---
